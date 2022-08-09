172eafa55d258994233d849effdada0db61d5b2c
Caleb Elliott <caleb123elliott@gmail.com>
2022-08-09 11:43:02 -0600

    build

---
+++ landingPage/wwwroot/assets/AboutPage.67aa13dd.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.04c89ff1.js";import{o,b as t,g as s}from"./vendor.7568c694.js";const a={name:"AboutPage"},c={class:"about"},n=s("h1",null,"This is the about page",-1),r=[n];function _(i,d,p,f,u,h){return o(),t("div",c,r)}var b=e(a,[["render",_]]);export{b as default};
